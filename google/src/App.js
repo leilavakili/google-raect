96a3a52a6f679fe054f1689e1ffb4d2621379a02
Leila Vakili <leilavakili2002@gmail.com>
2023-06-18 01:49:54 +0330

    3rd

--- google/src/App.js
+++ google/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import logo from './google.png';
 
 
 function App() {
   return (
     <div className="App">
+     <img src={logo} className="App-logo" alt="logo" />
     <Navbar />
     </div>
   );
--- google/src/components/Navbar/MenuItems.js
+++ google/src/components/Navbar/MenuItems.js
@@ -9,15 +9,5 @@ export const MenuItems = [
         title: 'Images',
         URL:'https://www.google.com/imghp?hl=en&ogbl',
         cName: 'nav-links'
-    },
-    {
-        title: 'Menu',
-        URL:'#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Sign in',
-        URL:'https://accounts.google.com/v3/signin/identifier?dsh=S-335396044%3A1687016516805978&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&hl=en&ifkv=Af_xneHPvc7w1F951gkPvSNYJt4BQhvHB-kjqMydfMNuAfcqXHKJZrNI8x02hBDEszI8ZEe5iJvQ2A&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
-        cName: 'nav-links-mobile'
-    },
+    }
 ]
