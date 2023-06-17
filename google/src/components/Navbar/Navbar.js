8153ccbc434ea0c5db34ec5919b53f7144d1f293
Leila Vakili <leilavakili2002@gmail.com>
2023-06-18 01:49:29 +0330

    3rd

--- google/src/components/Navbar/Navbar.js
+++ google/src/components/Navbar/Navbar.js
@@ -4,20 +4,25 @@ import'./Navbar.css'
 import { Button } from '../Button'
 
 class Navbar extends Component{
+
+}
   render(){
     return(
       <nav className="NavbarItems">
 
-          <div className="navbar-menu">menu</div>
-          <ul className="nav-menu">
-           {MenuItems.map((item, index)=>{
-             return(
-              <li key={index}><a className={Items.cName} href={item.URL}>{item.title}</a></li>
-             )
+          <div className="menu-icon">
+          <a ></a>
+          </div>
+            <ul className="nav-menu">
+              {MenuItems.map((item, index)=>{
+                return(
+                <li key={index}>
+                  <a className={item.cName} href={item.URL}>{item.title}</a>
+                </li>
+                )
            })}
-          
-          </ul>
-          <Button>Sign</Button>
+           </ul>
+          <Button>Sign in</Button>
       </nav>
     )
   }
