c065e71578acef056a568c0e9c3b4cbed690f191
Leila Vakili <leilavakili2002@gmail.com>
2023-06-18 13:47:12 +0330

    5th

--- google/src/App.js
+++ google/src/App.js
@@ -8,8 +8,27 @@ function App() {
   return (
     <div className="App">
     <Navbar />
+    <body>
+    <section className="section">
     <img src={logo} className="App-logo" alt="logo"></img>
+    <form>
+    <br></br>
+    <div class="search-b">
+
+    <img alt="magnifier" src="img/magnifier glass.png" class="magnifier"/>
+    <input type="text" class="search-input"/>
+    <img alt="mic" src="img/mic.png" class="mic-icon" />
+    <img alt="lens" src="img/lens.png" class="lens-icon"/>
+    <input type="submit" class="search-botton" value="Google Search"/>
+    <input type="submit" class="search-botton" value="I'm Feeling Lucky"/>
+
+</div>
+    </form>
+    </section>
+    </body>
     </div>
+    
+
   );
 }
 
