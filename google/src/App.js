d6eebcf302e38ac023484ea73e20229fcd366d7d
Leila Vakili <leilavakili2002@gmail.com>
2023-06-18 03:47:20 +0330

    4th

--- google/src/App.js
+++ google/src/App.js
@@ -7,8 +7,8 @@ import logo from './google.png';
 function App() {
   return (
     <div className="App">
-     <img src={logo} className="App-logo" alt="logo" />
     <Navbar />
+    <img src={logo} className="App-logo" alt="logo"></img>
     </div>
   );
 }
